0918a0ee8a867f5367bdbe85265a57a56972d023
muratoztin <muratoztin16@gmail.com>
2024-04-13 13:13:00 +0300

    Add project files.

---
+++ MVCSTOK/Controllers/KategoriController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCSTOK.Models;
+using PagedList;
+using PagedList.Mvc;
+
+namespace MVCSTOK.Controllers
+{
+    public class KategoriController : Controller
+    {
+        // GET: Kategori
+        MvcDbStokEntities db = new MvcDbStokEntities();
+        public ActionResult Index(int sayfa=1)
+        {
+            //var degerler = db.TBLKATEGORILER.ToList();
+
+            //Buradaki sayfa--> başlangıç değeri(kaçıncı sayfadan başlasın) 4---> her listede kaç değer gelsin
+            var degerler = db.TBLKATEGORILER.ToList().ToPagedList(sayfa, 4);
+
+            return View(degerler);
+        }
+
+        //Eğer ben bir değişiklik yapmazsam veya
+        //sayfayı ilk kez çağırıyorsam sadece view döndür.
+        [HttpGet]
+        public ActionResult YeniKategori()
+        {           
+            return View();
+        }
+
+
+        //Butona tıkladığımda bunu getir
+        [HttpPost]
+        public ActionResult YeniKategori(TBLKATEGORILER p1)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("YeniKategori");
+            }
+            db.TBLKATEGORILER.Add(p1);
+            db.SaveChanges();
+            return View();
+        }
+        public ActionResult SIL(int id)
+        {
+            var kategori=db.TBLKATEGORILER.Find(id);
+            db.TBLKATEGORILER.Remove(kategori);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult KategoriGetir(int id)
+        {
+            var ktgr = db.TBLKATEGORILER.Find(id);
+            return View("KategoriGetir",ktgr);
+        }
+        
+        public ActionResult Guncelle(TBLKATEGORILER p1)
+        {
+            var ktg = db.TBLKATEGORILER.Find(p1.KATEGORIID);
+            ktg.KATEGORIAD = p1.KATEGORIAD;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MVCSTOK/Controllers/SatisController.cs
@@ -0,0 +1,31 @@
+﻿using MVCSTOK.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCSTOK.Controllers
+{
+    public class SatisController : Controller
+    {
+        // GET: Satis
+        MvcDbStokEntities db=new MvcDbStokEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult YeniSatis()
+        { 
+            return View(); 
+        }
+        [HttpPost]
+        public ActionResult YeniSatis(TBLSATISLAR p)
+        {
+            db.TBLSATISLAR.Add(p);
+            db.SaveChanges();
+            return View("Index");
+        }
+    }
+}
